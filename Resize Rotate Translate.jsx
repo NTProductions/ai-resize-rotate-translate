f17b46bf126a03464d9c313aa23edc691a11b2db
Nate Lovell <Natelovell99@gmail.com>
2020-10-04 23:48:06 -0600

    Create Resize Rotate Translate.jsx

---
+++ Resize Rotate Translate.jsx
@@ -0,0 +1,23 @@
+﻿var document = app.documents.add();
+document.documentColorSpace = DocumentColorSpace.RGB;
+var layer = document.layers[0];
+
+var fillColour = new RGBColor();
+fillColour.red = 230;
+fillColour.green = 50;
+fillColour.blue = 0;
+
+// for rectangle (top, left, width, height)
+var rectangle = layer.pathItems.rectangle(document.height*.5+75, document.width*.5-75, 150, 150);
+rectangle.fillColor = fillColour;
+rectangle.filled = true;
+
+// resize
+rectangle.resize(350, 350);
+
+// rotate
+rectangle.rotate(45);
+
+// translate
+// (deltaX, deltaY)
+rectangle.translate(100, 50);
